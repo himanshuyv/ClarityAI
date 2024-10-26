071bb5c79659a45c3b322f42890b3e086b2e7332
gitPROhan <r9901452726@gmail.com>
2024-10-27 03:14:04 +0530

    refactor: extractor class made and integrated

--- models/main.py
+++ models/main.py
@@ -2,15 +2,25 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from polarity import PolarityFinder
+from extractor import KeywordExtractor
+
 data = pd.read_csv('../data/synthetic.csv').sample(frac=1, random_state=42)
-x = data['User Input']
-y = data['Polarity']
+x_user = data['User Input']
+x_concern = data['Extracted Concern']
+y_polarity = data['Polarity']
+y_concern = data['Extracted Concern']
 
-x_train, x_temp, y_train, y_temp = train_test_split(x, y, test_size=0.3, random_state=42)  # 70% train, 30% temp
-x_val, x_test, y_val, y_test = train_test_split(x_temp, y_temp, test_size=0.5, random_state=42)  # Split 30% into 15% val and 15% test
+# x_train_polarity, x_temp_polarity, y_train_polarity, y_temp_polarity = train_test_split(x_user, y_polarity, test_size=0.3, random_state=42)  # 70% train, 30% temp
+# x_val_polarity, x_test_polarity, y_val_polarity, y_test_polarity = train_test_split(x_temp_polarity, y_temp_polarity, test_size=0.5, random_state=42)  # Split 30% into 15% val and 15% test
+# polarity_model = PolarityFinder()
+# polarity_model.fit(x_train_polarity, y_train_polarity, x_val_polarity, y_val_polarity, x_test_polarity, y_test_polarity)
+# polarity_accuracy = polarity_model.get_accuracy()
+# print("Polarity Accuracy Results:", polarity_accuracy)
 
-polarity_model = PolarityFinder()
-polarity_model.fit(x_train, y_train, x_val, y_val, x_test, y_test)
-
-polarity_accuracy = polarity_model.get_accuracy()
-print("Accuracy Results:", polarity_accuracy)
+x_train_extractor, x_temp_extractor, y_train_extractor, y_temp_extractor = train_test_split(x_user, y_concern, test_size=0.3, random_state=42)  # 70% train, 30% temp
+x_val_extractor, x_test_extractor, y_val_extractor, y_test_extractor = train_test_split(x_temp_extractor, y_temp_extractor, test_size=0.5, random_state=42)  # Split 30% into 15% val and 15% test
+ner_model = KeywordExtractor()
+ner_model.fit(x_train_extractor, y_train_extractor, x_val_extractor, y_val_extractor, n_iter=10)
+ner_model.load_model()
+test_accuracy = ner_model.evaluate_accuracy(x_test_extractor, y_test_extractor)
+print(f"Extractor Test Accuracy: {test_accuracy:.2%}")
